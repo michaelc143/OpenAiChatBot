56305f29a849a6567b01e4618a858700127f02ec
Michael Corbishley <79810775+michaelc143@users.noreply.github.com>
2023-03-22 11:15:30 -0500

    implemented image mode with btn to select mode

--- src/ChatBot.js
+++ src/ChatBot.js
@@ -14,27 +14,40 @@ function ChatBot() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    var imageChecked = false; //true if image btn is selected
+    //check if box is checked
+    document.getElementById('img-btn').onclick = function() {
+      imageChecked = true;
+    };
     
-    //For image creation
-    //const response = await openai.createImage({
-    //  prompt:input,
-    //  n:1, (n is number of imgs)
-    //  size="1024x1024", (size of output imgs)
-    //});
-    //setResponse(response.data.data[0].url);
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: input,
-      max_tokens: 50,
-      temperature: 0,
-    });
+    //img generation mode
+    if(imageChecked) {
+      //For image creation
+      //const response = await openai.createImage({
+      //  prompt:input,
+      //  n:1, (n is number of imgs)
+      //  size="1024x1024", (size of output imgs)
+      //});
+      //setResponse(response.data.data[0].url);
+    }
+    
+    //text chat generation mode
+    else {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: input,
+        max_tokens: 50,
+        temperature: 0,
+      });
 
-    setResponse(response.data.choices[0].text);
+      setResponse(response.data.choices[0].text); 
+    }
   };
 
   return (
     <div id='page'>
       <form onSubmit={handleSubmit}>
+        <button id='img-btn' type='checkbox'>Select checkbox for image generation mode</button>
         <label>
           Input:
           <input className='chat-input-box' type="text" value={input} onChange={(event) => setInput(event.target.value)} />
