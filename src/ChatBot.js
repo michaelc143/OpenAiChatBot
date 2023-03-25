d20af33a2aa331f5c2c36dea1dad79fad1b02ce3
Michael Corbishley <79810775+michaelc143@users.noreply.github.com>
2023-03-25 15:12:42 -0500

    works with a checkbox to select image or chat mode now

--- src/ChatBot.js
+++ src/ChatBot.js
@@ -11,50 +11,42 @@ function ChatBot() {
   const openai = new OpenAIApi(configuration);
   const [input, setInput] = useState('');
   const [response, setResponse] = useState('');
+  const [useImageBtn, setUseImageBtn] = useState(false);
 
+  const handleImage = async (event) => {
+    event.preventDefault();
+    const response = await openai.createImage({
+      prompt: input,
+      n: 1,
+      size: "1024x1024",
+    });
+    setResponse(response.data.data[0].url);
+  };
+    
   const handleSubmit = async (event) => {
     event.preventDefault();
-    var imageChecked = false; //true if image btn is selected
-    //check if box is checked
-    document.getElementById('img-btn').onclick = function() {
-      imageChecked = true;
-    };
-    
-    //img generation mode
-    if(imageChecked) {
-      //For image creation
-      //const response = await openai.createImage({
-      //  prompt:input,
-      //  n:1, (n is number of imgs)
-      //  size="1024x1024", (size of output imgs)
-      //});
-      //setResponse(response.data.data[0].url);
-    }
-    
-    //text chat generation mode
-    else {
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: input,
-        max_tokens: 50,
-        temperature: 0,
-      });
-
-      setResponse(response.data.choices[0].text); 
-    }
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: input,
+      max_tokens: 500,
+      temperature: 0,
+    });
+    setResponse(response.data.choices[0].text);
   };
+    
+  const submissionType = useImageBtn ? handleImage : handleSubmit;
 
   return (
     <div id='page'>
-      <form onSubmit={handleSubmit}>
-        <button id='img-btn' type='checkbox'>Select checkbox for image generation mode</button>
+      Image btn<input type='checkbox' checked={useImageBtn} onChange={() => setUseImageBtn(!useImageBtn)}></input>
+      <form onSubmit={submissionType}>
         <label>
           Input:
           <input className='chat-input-box' type="text" value={input} onChange={(event) => setInput(event.target.value)} />
         </label>
-        <button type="submit">Submit</button>
+        <button type="submit" className='submit-btn'>Submit</button>
       </form>
-      <p>Response: {response}</p>
+      <p className='response-field'>Response: {response}</p>
     </div>
   );
 }
